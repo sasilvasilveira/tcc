a6549c4dade383b3bf0e61b8cf5eed5b10f0b867
sasilvasilveira <srhsilveira10@gmail.com>
2024-01-10 15:29:54 -0400

    Updated gitignore file

--- Implementation/main.py
+++ Implementation/main.py
@@ -2,18 +2,19 @@ from src.db_clean import CleanDatabase
 from src.algorithms import Algorithms
 from src.constants import BUG_CATEGORY_COLUMN_NAME, COLUMNS_TO_REMOVE
 
+
 def main():
     clean_database = CleanDatabase(
-        'src/amostra-bugs.csv',
-        BUG_CATEGORY_COLUMN_NAME,
-        COLUMNS_TO_REMOVE
+        "src/bugs-2023.csv", BUG_CATEGORY_COLUMN_NAME, COLUMNS_TO_REMOVE
     )
     clean_database.clean_database_process()
 
     algorithms = Algorithms(clean_database.data)
     algorithms.create_bug_instance_list()
 
+    algorithms.naive_bayes()
     algorithms.redes_neurais_recorrentes()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
